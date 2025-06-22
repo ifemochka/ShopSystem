477af2e7e021b7fedebff664942b6628eea3044d
Алёна Лейбович <almaksi@outlook.com>
2025-06-22 19:30:56 +0300

    Исправление

---
+++ OrdersService/Data/OrdersDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using OrdersService.Data;
+
+public class OrdersDbContextFactory : IDesignTimeDbContextFactory<OrdersDbContext>
+{
+    public OrdersDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<OrdersDbContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=ordersdb;Username=postgres;Password=postgres");
+
+        return new OrdersDbContext(optionsBuilder.Options);
+    }
+}
